7fd06ed7fb8ac3085549cd62194c752ca30d1a12
Cameron Earl <developer.cameron@gmail.com>
2017-10-13 10:24:45 -0700

    Implement create user

--- app.js
+++ app.js
@@ -4,15 +4,38 @@ const knex = require('knex')(config);
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 const port = process.env.PORT || 8000;
-app.use(bodyParser.json({ extended: true }));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=> {
-  res.render('index.ejs');
+app.get('/users', (req, res) => {
+  knex('users').then((response)=>{
+    res.render('users', {users: response});
+  });
 });
 
-app.use('/', (req, res)=>{
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  knex('users')
+    .insert(req.body, '*')
+    .then((result) => {
+      res.redirect('/users');
+    })
+    .catch((err) => {
+      console.error(err);
+      // res.sendStatus(400);
+    });
+
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('/', (req, res) => {
   res.sendStatus(418);
 });
 
---
+++ public/js/users.js
@@ -0,0 +1,6 @@
+$(function onReady() {
+  $('.new-user-button').click(()=>{
+    $('#new-user-form').toggle();
+  });
+  $('#new-user-form').hide();
+});
